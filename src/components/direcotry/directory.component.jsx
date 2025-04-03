c29b1b95dc5598814a4b4fc7e91b6e7d78774bf5
Danish munawar <dmunawar39@gmail.com>
2025-04-03 03:06:59 +0300

    setup the project and added directory and catagory item components

---
+++ src/components/direcotry/directory.component.jsx
@@ -0,0 +1,12 @@
+import CatagoryItem from "../catagory-item/catagory.component";
+import "./directory.styles.scss";
+const Directory = ({ catagories }) => {
+  return (
+    <div className="directory-container">
+      {catagories.map((catagory) => (
+        <CatagoryItem key={catagory.id} catagory={catagory} />
+      ))}
+    </div>
+  );
+};
+export default Directory;
