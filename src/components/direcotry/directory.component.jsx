707097c9912cd2983b9574057ddfd2e52ff5a0b7
Danish munawar <dmunawar39@gmail.com>
2025-04-04 10:36:22 +0300

    Saving my work before pulling

--- src/components/direcotry/directory.component.jsx
+++ src/components/direcotry/directory.component.jsx
@@ -2,6 +2,7 @@ import CatagoryItem from "../catagory-item/catagory.component";
 import "./directory.styles.scss";
 const Directory = ({ catagories }) => {
   return (
+    //  Directory component renders a list of category items
     <div className="directory-container">
       {catagories.map((catagory) => (
         <CatagoryItem key={catagory.id} catagory={catagory} />
